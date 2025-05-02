168fc32fd94bcfb7b0b4415a939146550d73b666
MitzyRomero <mitzyromerochacon@gmail.com>
2025-05-02 04:21:55 -0600

    Update asistente_pymes_streamlit.py

--- asistente_pymes_streamlit.py
+++ asistente_pymes_streamlit.py
@@ -44,8 +44,7 @@ def generar_pdf(nombre_empresa, municipio, nivel, recomendacion):
     pdf.cell(200, 10, txt=f"Municipio: {municipio}", ln=True)
     pdf.cell(200, 10, txt=f"Nivel de riesgo detectado: {nivel}", ln=True)
     pdf.ln(10)
-    pdf.multi_cell(0, 10, txt=f"Recomendación:
-{recomendacion}")
+    pdf.multi_cell(0, 10, txt=f"Recomendación:\n{recomendacion}")
     nombre_archivo = f"Informe_{nombre_empresa.replace(' ', '_')}.pdf"
     ruta = os.path.join("/tmp", nombre_archivo)
     pdf.output(ruta)
